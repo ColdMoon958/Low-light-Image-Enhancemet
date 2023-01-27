248965f66b4a2b6436388d930789090096100aed
ColdMoon958 <119122797+ColdMoon958@users.noreply.github.com>
2023-01-27 22:14:56 +0530

    Add files via upload

---
+++ code/Zero-DCE_modified/DCP_loss function/DCP_loss/train_model.py
@@ -0,0 +1,241 @@
+'''
+MIT License
+
+Copyright (c) 2021 Tauhid Khan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import os
+import time
+from datetime import datetime
+from tqdm import tqdm
+import argparse
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+
+from ZeroDCE.dataset import TfdataPipeline
+from ZeroDCE.zero_dce import ZeroDCE
+from ZeroDCE.zero_dce_lite import ZeroDCE_lite
+from ZeroDCE.losses import SpatialConsistencyLoss, \
+                            ExposureControlLoss,   \
+                            ColorConstancyLosss,   \
+                            IlluminationSmoothnessLoss, \
+                            DCP_loss
+
+# TODO: Add support for Pre-trained model to be loaded from checkpoint directory and continue training
+
+tf.random.set_seed(4)
+__model_type = ['zero_dce', 'zero_dce_lite']
+
+
+def train(
+    dataset_dir: str,
+    checkpoint_dir: str,
+    IMG_H:int = 400,
+    IMG_W:int = 600,
+    IMG_C:int = 3,
+    batch_size:int = 8,
+    epoch:int = 50,
+    learning_rate:float = 1e-4,
+    dataset_split:float = 0.05,
+    logdir:str = 'logs/',
+    iteration:int = 8,
+    filters:int = 32,
+    model_type: str = 'zero_dce',
+):
+    assert os.path.isdir(dataset_dir), f'Dataset directory {dataset_dir} is not a directory'
+    assert os.path.exists(dataset_dir), f'Dataset directory {dataset_dir} does not exist'
+    assert 0 < dataset_split < 0.3, 'Dataset split must be between 0 and 0.3'
+    assert isinstance(model_type, str), 'Model Type should be a string'
+
+    if not os.path.exists(checkpoint_dir):
+        os.mkdir(checkpoint_dir)
+    
+    # Instantiate tf.summary FileWriter
+    logdir = f'{logdir}/Model/{model_type}_{datetime.now().strftime("%Y%m%d-%H%M%S")}'
+    train_writer = tf.summary.create_file_writer(logdir+'/train/')
+    val_writer = tf.summary.create_file_writer(logdir+'/val/')
+
+    # Instantiate dataset pipeline
+    tf.print('Creating dataset pipeline...\n')
+    tfdatapipeline = TfdataPipeline(
+        BASE_DATASET_DIR=dataset_dir,
+        IMG_H=IMG_H,
+        IMG_W=IMG_W,
+        IMG_C=IMG_C,
+        batch_size=batch_size,
+        split=dataset_split,
+    )
+    train_data = tfdatapipeline.data_loader(dataset_type='train')
+    val_data = tfdatapipeline.data_loader(dataset_type='valid')
+
+    # Instantiate Optimiizer
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    # Instantiating Losses
+    spatial_consistency_loss = SpatialConsistencyLoss()
+    exposure_control_loss = ExposureControlLoss(e=0.6, patch_size=8)
+    color_constancy_loss = ColorConstancyLosss()
+    illumination_smoothness_loss = IlluminationSmoothnessLoss()
+    dark_channel_prior_loss=DCP_loss()
+
+    # Instantiate Zero-DCE model
+    if model_type == 'zero_dce':
+        tf.print('Creating Zero-DCE model...\n')
+        model = ZeroDCE(
+            name='DCE-Net',
+            filters=filters,
+            iteration=iteration,
+            IMG_H=IMG_H,
+            IMG_W=IMG_W,
+            IMG_C=IMG_C,
+        )
+    elif model_type == 'zero_dce_lite':
+        tf.print('Creating Zero-DCE Lite model...\n')
+        model = ZeroDCE_lite(
+            name='DCE-Net_lite',
+            filters=filters,
+            iteration=iteration,
+            IMG_H=IMG_H,
+            IMG_W=IMG_W,
+            IMG_C=IMG_C,
+        )
+    else:
+        raise ValueError(f'Model type {model_type} is not supported')
+
+    # Compile the model
+    tf.print('Compiling the model...\n')
+    model.compile(
+        optimizer=optimizer,
+        spatial_consistency_loss=spatial_consistency_loss,
+        exposure_control_loss=exposure_control_loss,
+        color_constancy_loss=color_constancy_loss,
+        illumination_smoothness_loss=illumination_smoothness_loss,
+        dark_channel_prior_loss=dark_channel_prior_loss
+    )
+
+    tf.print(f'[INFO] Summary of model\n')
+    tf.print(model.summary())
+
+    tf.print('\n')
+    tf.print('*'*60)
+    tf.print('\t\t\tModel Configs')
+    tf.print('*'*60)
+
+    tf.print(
+        f'\n',
+        f'Model Name                : {model.name}\n',
+        f'Input shape               : ({IMG_H, IMG_W, IMG_C})\n',
+        f'Epochs                    : {epoch}\n',
+        f'Batch Size                : {batch_size}\n',
+        f'Learning Rate             : {learning_rate}\n',
+        f'Dataset Split             : {dataset_split}\n',
+        f'Post enhancing Iteration  : {iteration}\n',
+        f'\n',
+    )
+
+    # Training the model
+    tf.print('Training the model...\n')
+    for e in range(epoch):
+        t = time.time()
+
+        for img in tqdm(train_data, unit='steps', desc='training...', colour='red'):
+            losses = model.train_step(img)
+        
+        for img in tqdm(val_data, unit='steps', desc='validating...', colour='green'):
+            val_losses = model.test_step(img)
+        
+        tf.print(f"ETA:{round((time.time() - t)/60, 2)} - epoch: {(e+1)} - loss: {losses['total_loss']}  val_loss: {val_losses['total_loss']}\n")
+
+        tf.print('Writing logs to TensorBoard...\n')
+        with train_writer.as_default():
+            print(f"loss: {losses['total_loss']}")
+            print(f"DCP-loss: {losses['dark_channel_prior_loss']}")
+            print(f"spatial_consistency_loss: {losses['spatial_consistency_loss']}")
+            print(f"exposure_control_loss: {losses['exposure_control_loss']}")
+            print(f"color_constancy_loss: {losses['color_constancy_loss']}")
+            print(f"illumination_smoothness_loss: {losses['illumination_smoothness_loss']}\n\n")
+
+
+            tf.summary.scalar('loss', losses['total_loss'], step=e+1)
+            tf.summary.scalar('spatial_consistency_loss', losses['spatial_consistency_loss'], step=e+1)
+            tf.summary.scalar('exposure_control_loss', losses['exposure_control_loss'], step=e+1)
+            tf.summary.scalar('color_constancy_loss', losses['color_constancy_loss'], step=e+1)
+            tf.summary.scalar('illumination_smoothness_loss', losses['illumination_smoothness_loss'], step=e+1)
+        
+        with val_writer.as_default():
+            print(f"val_loss: {val_losses['total_loss']}")
+            print(f"val_DCP-loss: {val_losses['dark_channel_prior_loss']}")
+            print(f"val_spatial_consistency_loss: {val_losses['spatial_consistency_loss']}")
+            print(f"val_exposure_control_loss: {val_losses['exposure_control_loss']}")
+            print(f"val_color_constancy_loss: {val_losses['color_constancy_loss']}")
+            print(f"val_illumination_smoothness_loss: {val_losses['illumination_smoothness_loss']}\n\n")
+
+            tf.summary.scalar('va_loss', val_losses['total_loss'], step=e+1)
+            tf.summary.scalar('val_spatial_consistency_loss', val_losses['spatial_consistency_loss'], step=e+1)
+            tf.summary.scalar('val_exposure_control_loss', val_losses['exposure_control_loss'], step=e+1)
+            tf.summary.scalar('val_color_constancy_loss', val_losses['color_constancy_loss'], step=e+1)
+            tf.summary.scalar('val_illumination_smoothness_loss', val_losses['illumination_smoothness_loss'], step=e+1)
+        
+        if (e+1)%10 == 0:
+            tf.print(f'Saving model at epoch {e+1}...\n')
+            model.save(f'{checkpoint_dir}/{model_type}_iter{iteration}/{model_type}_{IMG_H}x{IMG_W}_iter{iteration}_{e+1}', save_format='tf')
+            tf.print(f'Saved model at epoch {e+1}\n')
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Model training scipt for Zero-DCE models')
+    parser.add_argument('--dataset_dir', type=str, required=True, help='Dataset directory')
+    parser.add_argument('--checkpoint_dir', type=str, default='Trained_Model/', help='Checkpoint directory')
+    parser.add_argument('--model_type', type=str, default='zero_dce', help=f'Type of Model.should be any of: {__model_type}')
+    parser.add_argument('--IMG_H', type=int, default=400, help='Image height')
+    parser.add_argument('--IMG_W', type=int, default=600, help='Image width')
+    parser.add_argument('--IMG_C', type=int, default=3, help='Image channels')
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
+    parser.add_argument('--epoch', type=int, default=50, help='Epochs')
+    parser.add_argument('--learning_rate', type=float, default=1e-4, help='Learning rate')
+    parser.add_argument('--dataset_split', type=float, default=0.05, help='Dataset split')
+    parser.add_argument('--logdir', type=str, default='logs/', help='Log directory')
+    parser.add_argument('--filters', type=int, default=32, help='Number of filters')
+    parser.add_argument('--iteration', type=int, default=8, help='Post enhancing iteration')
+    args = parser.parse_args()
+
+    train(
+        dataset_dir=args.dataset_dir,
+        checkpoint_dir=args.checkpoint_dir,
+        model_type=args.model_type,
+        IMG_H=args.IMG_H,
+        IMG_W=args.IMG_W,
+        IMG_C=args.IMG_C,
+        batch_size=args.batch_size,
+        epoch=args.epoch,
+        learning_rate=args.learning_rate,
+        dataset_split=args.dataset_split,
+        logdir=args.logdir,
+        filters=args.filters,
+        iteration=args.iteration,
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/Zero-DCE_modified/Image_Enhancement_Eqn_modified/IE_eqn_dcp/zero_dce.py
@@ -0,0 +1,195 @@
+'''
+MIT License
+
+Copyright (c) 2021 Tauhid Khan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import os
+from typing import Tuple
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+
+
+class ZeroDCE(tf.keras.Model):
+
+    def __init__(self, name:str = "DCE-net", filters:int = 32, iteration: int = 8, IMG_H:int = 384, IMG_W:int =512, IMG_C:int = 3, **kwargs):
+        super(ZeroDCE, self).__init__(name=name, **kwargs)
+        self.filters = filters
+        self.iteration = iteration
+        self.IMG_H = IMG_H
+        self.IMG_W = IMG_W
+        self.IMG_C = IMG_C
+
+        self.conv1 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.conv2 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.conv3 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.conv4 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.concat_3_4 = tf.keras.layers.Concatenate(axis=-1)
+        self.conv5 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.concat_2_5 = tf.keras.layers.Concatenate(axis=-1)
+        self.conv6 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.concat_1_6 = tf.keras.layers.Concatenate(axis=-1)
+        self.a_map_conv = tf.keras.layers.Conv2D(self.iteration*3, kernel_size=(3, 3), strides=(1,1), padding='same',activation='tanh')
+    
+    def call(self, inputs: tf.Tensor)-> Tuple[tf.Tensor,...]:
+        x1 = self.conv1(inputs)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+        x_concat_3_4 = self.concat_3_4([x3, x4])
+        x_5 = self.conv5(x_concat_3_4)
+        x_concat_2_5 = self.concat_2_5([x2, x_5])
+        x_6 = self.conv6(x_concat_2_5)
+        x_concat_1_6 = self.concat_1_6([x1, x_6])
+        a_maps = self.a_map_conv(x_concat_1_6)
+
+        # Enhancing input image iteratively 
+        a_maps_splited = tf.split(a_maps, self.iteration, axis=-1)
+        le_img = inputs
+        size=17
+        img=tf.math.reduce_mean(le_img,axis=-1,keepdims=True)
+        print(f"img_mean shape: {img.shape}")
+        kernel=tf.zeros((size,size,1))
+        dcp=tf.nn.erosion2d(img, filters=kernel, strides=(1,1,1,1),data_format='NHWC', dilations=(1,1,1,1),padding="SAME")
+        print(f"dcp shape: {dcp.shape}")
+        for a_map in a_maps_splited:
+            le_img = le_img + a_map * (tf.square(le_img) - le_img)*(1-dcp)
+        return le_img, a_maps
+    
+    def compile(
+        self,
+        optimizer: tf.keras.optimizers.Optimizer,
+        spatial_consistency_loss: tf.keras.losses.Loss,
+        exposure_control_loss: tf.keras.losses.Loss,
+        color_constancy_loss: tf.keras.losses.Loss,
+        illumination_smoothness_loss: tf.keras.losses.Loss,
+        loss_weights: dict = {
+            'spatial_consistency_w': 1.0,
+            'exposure_control_w': 20.0,
+            'color_constancy_w': 10.0,
+            'illumination_smoothness_w': 400.0
+        },
+        **kwargs
+    ):
+        super(ZeroDCE, self).compile(**kwargs)
+        self.optimizer = optimizer
+        self.spatial_consistency_loss = spatial_consistency_loss
+        self.exposure_control_loss = exposure_control_loss
+        self.color_constancy_loss = color_constancy_loss
+        self.illumination_smoothness_loss = illumination_smoothness_loss
+        self.loss_weights = loss_weights
+    
+    def compute_losses(self, input_img:tf.Tensor, enhanced_img: tf.Tensor, a_maps: tf.Tensor)-> dict:
+        '''
+        Compute all zero reference DCE losses
+        args:
+            input_img: tf.Tensor, input image
+            enhanced_img: tf.Tensor, enhanced image
+            a_maps: tf.Tensor, Alpha maps of enhanced image
+        return:
+            dict, loss dictionary
+        '''
+        l_spa = self.loss_weights['spatial_consistency_w'] * self.spatial_consistency_loss(input_img,enhanced_img)
+        l_exp = self.loss_weights['exposure_control_w'] * self.exposure_control_loss(enhanced_img)
+        l_col = self.loss_weights['color_constancy_w'] * self.color_constancy_loss(enhanced_img)
+        l_ill = self.loss_weights['illumination_smoothness_w'] * self.illumination_smoothness_loss(a_maps)
+
+        total_loss = l_spa + l_exp + l_col + l_ill
+
+        return {
+            'total_loss': total_loss,
+            'spatial_consistency_loss': l_spa,
+            'exposure_control_loss': l_exp,
+            'color_constancy_loss': l_col,
+            'illumination_smoothness_loss': l_ill
+        }
+
+    @tf.function
+    def train_step(self, inputs: tf.Tensor) -> dict:
+        '''
+        Forward pass, calculate total loss, and calculate gradients with respect to loss.
+        args:
+            inputs: tf.Tensor, Tensor of shape (batch_size, IMG_H, IMG_W, IMG_C)
+        returns:
+            loss: tf.Tensor, Tensor of shape (batch_size, 1)
+        '''
+        with tf.GradientTape() as tape:
+            enhanced_img, a_maps = self(inputs)
+            losses = self.compute_losses(inputs, enhanced_img, a_maps)
+        
+        # Calculate gradients
+        gradients = tape.gradient(losses['total_loss'], self.trainable_variables)
+        # Backpropagate gradients to update weights.
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        return losses
+    
+    @tf.function
+    def test_step(self, inputs: tf.Tensor)-> dict:
+        '''
+        Forward pass, calculate total loss.
+        args:
+            inputs: tf.Tensor, Tensor of shape (batch_size, IMG_H, IMG_W, IMG_C)
+        returns:
+            dict, validation loss dictionary
+        '''
+        enahncened_img, a_maps = self(inputs)
+        val_losses = self.compute_losses(inputs, enahncened_img, a_maps)
+        return val_losses
+
+    def summary(self, plot:bool = False):
+        x = tf.keras.Input(shape=(self.IMG_H, self.IMG_W, self.IMG_C))
+        model = tf.keras.Model(inputs=[x], outputs=self.call(x), name='DCE-net')
+        if plot:
+            tf.keras.utils.plot_model(model, to_file='DCE-net.png', show_shapes=True, show_layer_names=True, rankdir='TB')
+        return model.summary()
+    
+    def get_config(self):
+        return {
+            'filters': self.filters,
+            'iteration': self.iteration
+        }
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+    
+
+if __name__ == "__main__":
+    from dataset import TfdataPipeline
+    
+    tfdataset = TfdataPipeline(BASE_DATASET_DIR='lol_datasetv2',batch_size=1)
+    test_data = tfdataset.data_loader('test')
+    model = ZeroDCE(filters=32, iteration=8)
+    tf.print(model.summary(plot=True))
+    tf.print(model.get_config())
+
+    for data in test_data.take(1):
+        y,a_maps = model(data)
+        # y = y * 255.0
+        a_maps = tf.split(a_maps, 8, axis=-1)
+
+        # a_maps = ((a_maps[-1] +1) / 2) * 255.0
+        a_maps = a_maps[-1]
+        tf.print(tf.reduce_min(y), tf.reduce_max(y))
+        tf.print(tf.reduce_min(a_maps), tf.reduce_max(a_maps))
+        tf.print(a_maps.shape)    
